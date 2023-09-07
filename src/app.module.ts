1ebbf73b8aebf23b479a5fd481bc898ba3ac1ed5
Lydie Chevalier <didof69@hotmail.fr>
2023-09-07 11:40:07 +0200

    config imports dans app.module et création .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,29 @@ import { RestrictionsModule } from './restrictions/restrictions.module';
 import { AlimentsModule } from './aliments/aliments.module';
 import { CategoriesModule } from './categories/categories.module';
 import { SaisonsModule } from './saisons/saisons.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UtilisateursModule, EnfantsModule, RestrictionsModule, AlimentsModule, CategoriesModule, SaisonsModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: [`.env`] }),
+    UtilisateursModule,
+    EnfantsModule,
+    RestrictionsModule,
+    AlimentsModule,
+    CategoriesModule,
+    SaisonsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      entities: [],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
