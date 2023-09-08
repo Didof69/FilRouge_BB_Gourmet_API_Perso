90ee3bf9aabb070127b2da3e65222b0da253d9eb
Lydie Chevalier <didof69@hotmail.fr>
2023-09-08 11:38:17 +0200

    CRUD aliment (sans validator) ok

--- src/aliments/dto/create-aliment.dto.ts
+++ src/aliments/dto/create-aliment.dto.ts
@@ -1,5 +1,8 @@
+import { Saison } from "src/saisons/entities/saison.entity";
+
 export class CreateAlimentDto {
     libelle: string;
     age_introduction: number;
     id_categorie: number;
+    saisons : Saison[]
 }
